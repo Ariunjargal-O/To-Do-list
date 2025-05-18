ee90841d29ccd890b1fd26ef7c7b98b56dab3d80
Ariunjargal Ochirpurev <ariunjargal.ochirpurev@gmail.com>
2025-05-18 13:37:38 +0800

    new

---
+++ step-2/src/app/page.js
@@ -0,0 +1,23 @@
+'use client'
+import styles from "./page.module.css";
+import Title from "./components/title/index.js";
+import Input from "./components/input/index.js";
+import Footer from "./components/footer/index.js"
+import Taskcontainer from "./components/TaskContainer";
+import { useState } from "react";
+
+
+
+export default function Home() {
+  const [tasks, setTasks] = useState([])
+  console.log(tasks)
+  return (
+    <div className={styles.cardContainer}>
+      <Title></Title>
+      {/* <Taskcontainer tasks={tasks} setTasks = {setTasks}></Taskcontainer> */}
+     <Input></Input>
+      <Footer></Footer>
+    <p></p>
+    </div>
+  );
+}
