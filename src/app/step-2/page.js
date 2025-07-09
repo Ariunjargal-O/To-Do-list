5d1bf0fe967c6ac41dc08a101af18be51d7b9b8d
Ariunjargal Ochirpurev <ariunjargal.ochirpurev@gmail.com>
2025-07-09 12:54:30 +0800

    status

--- src/app/step-2/page.js
+++ src/app/step-2/page.js
@@ -1,22 +1,22 @@
-'use client'
+// 'use client'
 
-import Title from "./components/title/index.js";
-import Input from "./components/input/index.js";
-import Footer from "./components/footer/index.js"
-import { useState } from "react";
+// import Title from "./components/title/index.js";
+// import Input from "./components/input/index.js";
+// import Footer from "./components/footer/index.js"
+// import { useState } from "react";
 
 
 
-export default function Home() {
-  const [tasks, setTasks] = useState([])
-  console.log(tasks)
-  return (
-    <div >
-      <Title></Title>
-      {/* <Taskcontainer tasks={tasks} setTasks = {setTasks}></Taskcontainer> */}
-     <Input></Input>
-      <Footer></Footer>
-    <p></p>
-    </div>
-  );
-}
+// export default function Home() {
+//   const [tasks, setTasks] = useState([])
+//   console.log(tasks)
+//   return (
+//     <div >
+//       <Title></Title>
+//       {/* <Taskcontainer tasks={tasks} setTasks = {setTasks}></Taskcontainer> */}
+//      <Input></Input>
+//       <Footer></Footer>git sta
+//     <p></p>
+//     </div>
+//   );
+// }
